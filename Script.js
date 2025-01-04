88b47f88f9722695a7521f7018cb6270210b9913
prit <132353280+Pritpatel11@users.noreply.github.com>
2025-01-04 22:56:29 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,44 @@
+var input = document.querySelector('input');
+var btn = document.querySelector('button');
+var h4 = document.querySelector('h4');
+var h2 = document.querySelector('h2');
+var count = 0;
+btn.addEventListener('click',function(){
+    h2.textContent = "Multiplication Table of:" + input.value;
+    
+    var num = input.value;
+    num = parseInt(num);
+    // console.log(num);
+  
+    if(count == 0){
+        for(var i=1;i<=10;i++){
+            h4.innerHTML += num + "*" + i + "=" + num*i + "<br>";
+            // console.log(num + "*" + i + "=" + num*i);
+        }
+        count++;
+    }
+    else{
+        console.log(count);
+        h4.innerHTML = "";
+        for(var i=1;i<=10;i++){
+            h4.innerHTML += num + "*" + i + "=" + num*i + "<br>";
+            // console.log(num + "*" + i + "=" + num
+        
+    }
+    
+}
+})
+
+// # Multiplication-Table-Genreter
+// A lightweight JavaScript application to generate multiplication tables for any number. This mini-project is an excellent way to explore fundamental programming concepts, DOM manipulation, and user interaction in JavaScript.
+
+// # Features
+// <li>Generates a multiplication table for any user-provided number.</li>
+// <li>Interactive and user-friendly interface.</li>
+// <li>Easily extendable for additional features or styling.</li>
+
+// # How to Use
+// <li>Clone the repository.</li>
+// <li>Open the index.html file in any modern web browser.</li>
+// <li>Input the desired number and range, and view the dynamically generated table.</li>
+// <li>This project is perfect for beginners and those looking to practice JavaScript, HTML, and CSS! 🌟</li>
